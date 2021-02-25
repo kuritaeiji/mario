650f91b96220ee21804e63b4c270dc96246c861e
kuritaeiji <eiji199825@yahoo.co.jp>
2021-02-26 01:57:26 +0900

    コイン

--- src/js/models/coin_block.js
+++ src/js/models/coin_block.js
@@ -1,4 +1,7 @@
+import consts from "../etcs/consts";
+import functions from "../etcs/functions";
 import vars from "../etcs/vars";
+import Coin from "./coin";
 
 export default class {
   constructor(spriteNum, mapNum) {
@@ -9,6 +12,9 @@ export default class {
 
   checkMarioCeilCollision(mapNum) {
     if (mapNum === this.mapNum) {
+      let coinX = functions.mapNumToX(mapNum) << 4;
+      let coinY = (functions.mapNumToY(mapNum) - consts.BLOCK_SIZE) << 4;
+      vars.field.coins.push(new Coin(coinX, coinY));
       vars.field.map[mapNum] = 373;
       this.kill = true;
     }
